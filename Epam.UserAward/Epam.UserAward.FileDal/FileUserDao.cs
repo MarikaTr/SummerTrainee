1c20d054622ace116d0b4a4fdef270f036204ef4
M5931 <m5931@student.jamk.fi>
2019-07-09 17:00:01 +0400

    with save in file(without implementation)

---
+++ Epam.UserAward/Epam.UserAward.FileDal/FileUserDao.cs
@@ -0,0 +1,32 @@
+﻿using Epam.UserAward.DalContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.UserAward.FileDal
+{
+    public class FileUserDao : IUserDao
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserAward.Entities.User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserAward.Entities.User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(UserAward.Entities.User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
